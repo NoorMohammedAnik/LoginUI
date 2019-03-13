8fa7e3e38db91cf9d684419e92f6bc5f9142ce31
Noor Mohammed Anik <noormohammedanik@gmail.com>
2019-03-14 01:24:30 +0600

    Initial commit

---
+++ app/src/main/java/com/anik/loginui/MainActivity.java
@@ -0,0 +1,22 @@
+package com.anik.loginui;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            Window w = getWindow();
+            w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        }
+
+        setContentView(R.layout.activity_main);
+    }
+}
